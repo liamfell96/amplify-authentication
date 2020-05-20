9ea6f35b0f9d0d44c4769cf1105bc83fb38244c4
Liam Fell <liam.haylands@hotmail.co.uk>
2020-05-20 21:26:37 +0100

    Set up AWS mobile client
    Added sign up and sign in activities

--- app/src/main/java/com/example/amplifyauthentication/MainActivity.java
+++ app/src/main/java/com/example/amplifyauthentication/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.amplifyauthentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.UserStateDetails;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails userStateDetails) {
+                Log.i("INIT", "onResult: " + userStateDetails.getUserState());
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e("INIT", "Initialization error.", e);
+
+            }
+        });
     }
 }
